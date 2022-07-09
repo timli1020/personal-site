3ac1a01a79d6253163283ad09c9021237c690926
timli1020 <zhaotian.li@uconn.edu>
2022-07-09 15:37:00 -0400

    install firebase and react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCPGVII9SER-3kbW52HGhyAEUWcA1UYEtE",
+  authDomain: "personalsite-d051e.firebaseapp.com",
+  projectId: "personalsite-d051e",
+  storageBucket: "personalsite-d051e.appspot.com",
+  messagingSenderId: "805463655630",
+  appId: "1:805463655630:web:93a0d4647c45062bee3c9f",
+  measurementId: "G-8RNZLYGNCE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   return (
     <div className="App">
