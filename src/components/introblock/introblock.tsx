99f8bdc9189a0cb52a8dfaf779976163cc930910
timli1020 <zhaotian.li@uconn.edu>
2023-05-26 12:49:39 -0400

    adds sunglasses emoji

--- src/components/introblock/introblock.tsx
+++ src/components/introblock/introblock.tsx
@@ -5,7 +5,10 @@ export default function IntroBlock () {
     return (
         <div className="intro-block">
             <h1 className="intro-block-text desc">Hello 👋! I'm </h1>
-            <h1 className="intro-block-text name">Tim Li</h1>
+            <div className="row">
+                <h1 className="intro-block-text name">Tim Li</h1>
+                <img className="poggers-icon" src="./techstackicons/sunglasses.jpeg" alt="logo" width={100} height={100}/>
+            </div>
             <h1 className="intro-block-text desc">A software engineer based in New York.</h1>
             <h1 className="intro-block-text desc">Nice to meet you!</h1>
         </div>
